5bfe208bfc7e086ad5fcd1d933051e28fc51b11e
Frank Fang <FrankFang1990@gmail.com>
2018-12-15 20:31:43 +0800

    完善 scroll.vue

--- src/demo.vue
+++ src/demo.vue
@@ -1,27 +1,185 @@
 <template>
-  <div>
-    {{selected}}
-    <div style="margin: 20px;">
-      <g-table :data-source="dataSource" bordered :selected-items.sync="selected" :order-by.sync="orderBy"
-        @update:orderBy="x" :loading="loading" :height="400" expend-field="description" checkable>
-        <g-table-column text="姓名" field="name" :width="100">
-          <template slot-scope="props">
-            <a :href="`/users/${props.value}`">{{props.value}}</a>
-          </template>
-        </g-table-column>
-        <g-table-column text="分数" field="score"></g-table-column>
-      </g-table>
-    </div>
+  <div class="page" style="position:relative;">
+    <div id="test" class="test">test</div>
+    <!--{{selected}}-->
+    <!--<div style="margin: 20px;">-->
+    <!--<g-table :data-source="dataSource" bordered :selected-items.sync="selected" :order-by.sync="orderBy"-->
+    <!--@update:orderBy="x" :loading="loading" :height="400" expend-field="description" checkable>-->
+    <!--<g-table-column text="姓名" field="name" :width="100">-->
+    <!--<template slot-scope="props">-->
+    <!--<a :href="`/users/${props.value}`">{{props.value}}</a>-->
+    <!--</template>-->
+    <!--</g-table-column>-->
+    <!--<g-table-column text="分数" field="score"></g-table-column>-->
+    <!--</g-table>-->
+    <!--</div>-->
+
+    <g-scroll style="width: 300px; height: 400px;">
+      <p>123456789_10_11_12_13_14_15_16_17_189</p>
+      <p>2</p>
+      <p>3</p>
+      <p>4</p>
+      <p>5</p>
+      <p>6</p>
+      <p>7</p>
+      <p>8</p>
+      <p>9</p>
+      <p>10</p>
+      <p>11</p>
+      <p>12</p>
+      <p>13</p>
+      <p>14</p>
+      <p>15</p>
+      <p>16</p>
+      <p>17</p>
+      <p>18</p>
+      <p>19</p>
+      <p>20</p>
+      <p>21</p>
+      <p>22</p>
+      <p>23</p>
+      <p>24</p>
+      <p>25</p>
+      <p>26</p>
+      <p>27</p>
+      <p>28</p>
+      <p>29</p>
+      <p>30</p>
+      <p>31</p>
+      <p>32</p>
+      <p>33</p>
+      <p>34</p>
+      <p>35</p>
+      <p>36</p>
+      <p>37</p>
+      <p>38</p>
+      <p>39</p>
+      <p>40</p>
+      <p>41</p>
+      <p>42</p>
+      <p>43</p>
+      <p>44</p>
+      <p>45</p>
+      <p>46</p>
+      <p>47</p>
+      <p>48</p>
+      <p>49</p>
+      <p>50</p>
+      <p>51</p>
+      <p>52</p>
+      <p>53</p>
+      <p>54</p>
+      <p>55</p>
+      <p>56</p>
+      <p>57</p>
+      <p>58</p>
+      <p>59</p>
+      <p>60</p>
+      <p>61</p>
+      <p>62</p>
+      <p>63</p>
+      <p>64</p>
+      <p>65</p>
+      <p>66</p>
+      <p>67</p>
+      <p>68</p>
+      <p>69</p>
+      <p>70</p>
+      <p>71</p>
+      <p>72</p>
+      <p>73</p>
+      <p>74</p>
+      <p>75</p>
+      <p>76</p>
+      <p>77</p>
+      <p>78</p>
+      <p>79</p>
+      <p>80</p>
+      <p>81</p>
+      <p>82</p>
+      <p>83</p>
+      <p>84</p>
+      <p>85</p>
+      <p>86</p>
+      <p>87</p>
+      <p>88</p>
+      <p>89</p>
+      <p>90</p>
+      <p>91</p>
+      <p>92</p>
+      <p>93</p>
+      <p>94</p>
+      <p>95</p>
+      <p>96</p>
+      <p>97</p>
+      <p>98</p>
+      <p>99</p>
+      <p>100</p>
+    </g-scroll>
+
   </div>
 </template>
+<style>
+  .page {
+    display: flex;
+    justify-content: center;
+  }
+  .parent {
+    height: 400px;
+    overflow: hidden;
+    width: 400px;
+    border: 5px solid red;
+    box-sizing: border-box;
+  }
+  .child {
+    border: 5px solid green;
+    box-sizing: border-box;
+  }
+  .test {
+    height: 100px; width: 100px; border: 1px solid red; position: absolute; top: 0; left: 0; z-index: 1;
+  }
+  .hide {
+    transform: translateX(-9999px);
+  }
+</style>
 <script>
   import GPager from './pager'
   import GTable from './table'
   import GTableColumn from './table-column'
+  import GScroll from './scroll'
 
+  let array = [
+    {name: '方方', score: 100, description: 'xxxx xxxx'},
+    {name: '圆圆', score: 99, description: 'yyyy yyyy'},
+    {name: '张三', score: 100},
+    {name: '李四', score: 99},
+    {name: '超人', score: 100},
+    {name: '蝙蝠侠', score: 99},
+    {name: '蜘蛛侠', score: 100},
+    {name: '钢铁侠', score: 99},
+    {name: '方方', score: 100},
+    {name: '圆圆', score: 99},
+    {name: '张三', score: 100},
+    {name: '李四', score: 99},
+    {name: '超人', score: 100},
+    {name: '蝙蝠侠', score: 99},
+    {name: '蜘蛛侠', score: 100},
+    {name: '钢铁侠', score: 99},
+    {name: '蜘蛛侠', score: 100},
+    {name: '钢铁侠', score: 99},
+    {name: '方方', score: 100},
+    {name: '圆圆', score: 99},
+  ]
+  let bigArray = []
+  // for (let i = 0; i < 5; i++) {
+  //   bigArray.push(...array.map((item, index) => {
+  //     return {id: i * 20 + index, ...item}
+  //   }))
+  // }
+  // console.log(bigArray)
   export default {
     name: "demo",
-    components: {GPager, GTable, GTableColumn},
+    components: {GPager, GTable, GTableColumn, GScroll},
     data () {
       return {
         currentPage: 1,
@@ -34,29 +192,38 @@
           score: 'desc'
         },
         loading: false,
-        dataSource: [
-          {id: 1, name: '方方', score: 100, description: 'xxxx xxxx'},
-          {id: 2, name: '圆圆', score: 99, description: 'yyyy yyyy'},
-          {id: 3, name: '张三', score: 100},
-          {id: 4, name: '李四', score: 99},
-          {id: 5, name: '超人', score: 100},
-          {id: 6, name: '蝙蝠侠', score: 99},
-          {id: 7, name: '蜘蛛侠', score: 100},
-          {id: 8, name: '钢铁侠', score: 99},
-          {id: 9, name: '方方', score: 100},
-          {id: 10, name: '圆圆', score: 99},
-          {id: 11, name: '张三', score: 100},
-          {id: 12, name: '李四', score: 99},
-          {id: 13, name: '超人', score: 100},
-          {id: 14, name: '蝙蝠侠', score: 99},
-          {id: 15, name: '蜘蛛侠', score: 100},
-          {id: 16, name: '钢铁侠', score: 99},
-          {id: 17, name: '蜘蛛侠', score: 100},
-          {id: 18, name: '钢铁侠', score: 99},
-          {id: 19, name: '方方', score: 100},
-          {id: 20, name: '圆圆', score: 99},
-        ]
+        // dataSource: bigArray
       }
+    },
+    mounted () {
+      let test = document.querySelector('#test')
+      let startPosition
+      let endPosition
+      let isMoving = false
+      let translateX = 0
+      let translateY = 0
+      test.addEventListener('mousedown', (e) => {
+        isMoving = true
+        let {screenX, screenY} = e
+        startPosition = {x: screenX, y: screenY}
+      })
+      test.addEventListener('selectstart', (e) => {e.preventDefault()})
+      document.addEventListener('mousemove', (e) => {
+        if (!isMoving) { return }
+        let {screenX, screenY} = e
+        endPosition = {x: screenX, y: screenY}
+        // delta = end - start
+        let delta = {x: endPosition.x - startPosition.x, y: endPosition.y - startPosition.y}
+        translateX = parseInt(translateX) + delta.x
+        translateY = parseInt(translateY) + delta.y
+        startPosition = endPosition
+        test.style.transform = `translate(0px,${translateY}px)`
+
+        console.log('move')
+      })
+      document.addEventListener('mouseup', (e) => {
+        isMoving = false
+      })
     },
     methods: {
       edit (item) {
---
+++ src/scroll.vue
@@ -0,0 +1,164 @@
+<template>
+  <div ref="parent" class="gulu-scroll-wrapper" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave" @wheel="onWheel"
+    @mousemove="onMouseMove">
+    <div ref="child" class="gulu-scroll" :style="{transform: `translateY(${this.contentY}px)`}">
+      <slot></slot>
+    </div>
+    <div class="gulu-scroll-track" v-show="scrollBarVisible">
+      <div class="gulu-scroll-bar" ref="bar" @mousedown="onMouseDownScrollBar" @selectstart="onSelectStartScrollBar">
+        <div class="gulu-scroll-bar-inner"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GuluScroll",
+    data () {
+      return {
+        scrollBarVisible: false,
+        isScrolling: false,
+        startPosition: undefined,
+        endPosition: undefined,
+        scrollBarY: 0,
+        contentY: 0,
+        barHeight: undefined,
+        parentHeight: undefined,
+        childHeight: undefined,
+        mouseIn: false,
+      }
+    },
+    beforeDestroy () {
+      // 移除 document 的事件监听
+    },
+    mounted () {
+      this.listenToDocument()
+      this.parentHeight = this.$refs.parent.getBoundingClientRect().height
+      this.childHeight = this.$refs.child.getBoundingClientRect().height
+      this.updateScrollBar()
+    },
+    computed: {
+      maxScrollHeight () {
+        return this.parentHeight - this.barHeight
+      }
+    },
+    methods: {
+      listenToDocument () {
+        document.addEventListener('mousemove', e => this.onMouseMoveScrollbar(e))
+        document.addEventListener('mouseup', e => this.onMouseUpScrollbar(e))
+      },
+      calculateContentYFromDeltaY (deltaY) {
+        let contentY = this.contentY
+        if (deltaY > 20) {
+          contentY -= 20 * 3
+        } else if (deltaY < -20) {
+          contentY -= -20 * 3
+        } else {
+          contentY -= deltaY * 3
+        }
+        return contentY
+      },
+      onWheel (e) {
+        this.updateContentY(e.deltaY, () => e.preventDefault())
+        this.updateScrollBar()
+      },
+      updateContentY (deltaY, fn) {
+        let maxHeight = this.calculateContentYMax()
+        this.contentY = this.calculateContentYFromDeltaY(deltaY)
+        if (this.contentY > 0) {
+          this.contentY = 0
+        } else if (this.contentY < -maxHeight) {
+          this.contentY = -maxHeight
+        } else {
+          fn && fn()
+        }
+      },
+      calculateContentYMax () {
+        let {borderTopWidth, borderBottomWidth, paddingTop, paddingBottom} = window.getComputedStyle(this.$refs.parent)
+        borderTopWidth = parseInt(borderTopWidth)
+        borderBottomWidth = parseInt(borderBottomWidth)
+        paddingTop = parseInt(paddingTop)
+        paddingBottom = parseInt(paddingBottom)
+        let maxHeight = this.childHeight - this.parentHeight + (borderTopWidth + borderBottomWidth + paddingTop + paddingBottom)
+        return maxHeight
+      },
+      updateScrollBar () {
+        let parentHeight = this.parentHeight
+        let childHeight = this.childHeight
+        this.barHeight = parentHeight * parentHeight / childHeight
+        this.$refs.bar.style.height = this.barHeight + 'px'
+        this.scrollBarY = -parentHeight * this.contentY / childHeight
+        this.$refs.bar.style.transform = `translateY(${this.scrollBarY}px)`;
+      },
+      onMouseEnter () {
+        this.scrollBarVisible = true
+        this.mouseIn = true
+      },
+      onMouseLeave () {
+        this.mouseIn = false
+        if (!this.isScrolling) this.scrollBarVisible = false;
+      },
+      onMouseMove () {
+        this.mouseIn = true
+      },
+      onMouseDownScrollBar (e) {
+        this.isScrolling = true
+        let {screenX, screenY} = e
+        this.startPosition = {x: screenX, y: screenY}
+      },
+      onMouseMoveScrollbar (e) {
+        if (!this.isScrolling) { return }
+        this.endPosition = {x: e.screenX, y: e.screenY}
+        let delta = {x: this.endPosition.x - this.startPosition.x, y: this.endPosition.y - this.startPosition.y}
+        this.scrollBarY = this.calculateScrollBarY(delta)
+        this.contentY = -(this.childHeight * this.scrollBarY / this.parentHeight)
+        this.startPosition = this.endPosition
+        this.$refs.bar.style.transform = `translate(0px,${this.scrollBarY}px)`
+      },
+      calculateScrollBarY (delta) {
+        let newValue = parseInt(this.scrollBarY) + delta.y
+        if (newValue < 0) {
+          newValue = 0
+        } else if (newValue > this.maxScrollHeight) {
+          newValue = this.maxScrollHeight
+        }
+        return newValue
+      },
+      onMouseUpScrollbar (e) {
+        this.isScrolling = false
+        if (!this.mouseIn) {
+          this.scrollBarVisible = false
+        }
+      },
+      onSelectStartScrollBar (e) {
+        e.preventDefault()
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .gulu-scroll {
+    transition: transform 0.05s ease;
+    &-wrapper {
+      border: 1px solid red;
+      overflow: hidden;
+      position: relative;
+    }
+    &-track { position: absolute; top: 0; right: 0; width: 14px; height: 100%;
+      background: #FAFAFA; border-left: 1px solid #E8E7E8; opacity: 0.9;
+    }
+    &-bar {
+      position: absolute; top: -1px; left: 50%; height: 40px; width: 8px;
+      margin-left: -4px; padding: 4px 0;
+      &-inner {
+        height: 100%; border-radius: 4px; background: #C2C2C2;
+        &:hover {
+          background: #7D7D7D;
+        }
+      }
+    }
+  }
+</style>
+
